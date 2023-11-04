baf529a08f757ff88b99cc291b3094488756b644
Dylanson25 <dvillarreal0@ucol.mx>
2023-11-04 09:39:03 -0600

    Instalacion de paquetes material UI

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,65 @@
+import "./styles.scss";
+
+export const Triangle = () => {
+  return (
+    <svg
+      className="triangle"
+      width="1440"
+      height="162"
+      viewBox="0 0 1440 162"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1440 1H0V61C0 116.228 44.7715 161 100 161H1440V1Z"
+        fill="#5964E0"
+      />
+      <mask
+        id="mask0_0_4644"
+        style={{ maskType: "luminance" }}
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="1"
+        width="1440"
+        height="160"
+      >
+        <path
+          d="M1440 1H0V61C0 116.228 44.7715 161 100 161H1440V1Z"
+          fill="white"
+        />
+      </mask>
+      <g mask="url(#mask0_0_4644)">
+        <path
+          opacity="0.0813802"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M1163.89 -204.424C1185.01 -228.387 1224.21 -220.054 1233.77 -189.571L1347.88 174.592C1357.27 204.528 1330.92 233.479 1300.24 226.956L933.748 149.057C903.063 142.535 890.772 105.372 911.518 81.8396L1163.89 -204.424Z"
+          fill="white"
+        />
+        <path
+          opacity="0.0813802"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M254.539 -322.259C251.422 -354.051 284.25 -377.038 313.059 -363.235L657.22 -198.336C685.511 -184.781 688.718 -145.77 663.02 -127.776L356.104 87.1288C330.406 105.123 294.845 88.7662 291.783 57.5449L254.539 -322.259Z"
+          fill="white"
+        />
+        <path
+          opacity="0.0813802"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M-71.4609 72.7414C-74.5785 40.9489 -41.7496 17.9619 -12.9407 31.7652L331.22 196.664C359.511 210.219 362.718 249.23 337.02 267.224L30.1041 482.129C4.4064 500.123 -31.1552 483.766 -34.2168 452.545L-71.4609 72.7414Z"
+          fill="white"
+        />
+      </g>
+    </svg>
+  );
+};
+const Header = () => {
+  return (
+    <header className="header rounded-sm-0">
+      <Triangle />
+    </header>
+  );
+};
+
+export default Header;
