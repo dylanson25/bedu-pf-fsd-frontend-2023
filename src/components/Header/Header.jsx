09662dc29ee281782e7ec47c2d8995eee8fc6ad6
Dylanson25 <dvillarreal0@ucol.mx>
2023-11-22 23:15:34 -0600

    component header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./styles.scss";
 import { Triangle } from "./Triangle";
-import { AppBar, Toolbar, Typography } from "@mui/material";
-import { useSelector } from "react-redux";
-import { PeopleAlt, Email } from "@mui/icons-material";
+import { AppBar, Toolbar, Typography, Switch } from "@mui/material";
+import { PeopleAlt, Email, Nightlight, Brightness7 } from "@mui/icons-material";
 import { LinkButton } from "../";
+import { setDarkMode } from "../../redux/darkMode";
 
 const Header = () => {
   const { status, user } = useSelector((state) => state.auth);
+  const { DARK } = useSelector((state) => state.darkMode);
+  const [theme, setTheme] = useState(!!DARK);
+  const dispatch = useDispatch();
+
+  const onChangeTheme = (event) => {
+    setTheme(event.target.checked);
+    dispatch(setDarkMode(theme ? 0 : 1));
+  };
+
   return (
     <AppBar className="header rounded-sm-0">
       <Toolbar className="px-3 px-sm-5 px-lg-3 px-xl-0">
@@ -18,29 +29,36 @@ const Header = () => {
         >
           devjobs
         </Typography>
-        {status === "authenticated" ? (
-          <div>
-            <div className="d-flex">
-              <PeopleAlt />
-              <Typography className="ms-2" variant="p">
-                {user.fullName}
-              </Typography>
-            </div>
-            <hr />
-            <div className="d-flex">
-              <Email />
-              <Typography className="ms-2" variant="p">
-                {user.email}
-              </Typography>
-            </div>
+        <div>
+          {status === "authenticated" ? (
+            <>
+              <div className="d-flex">
+                <PeopleAlt />
+                <Typography className="ms-2" variant="p">
+                  {user.fullName}
+                </Typography>
+              </div>
+              <hr />
+              <div className="d-flex">
+                <Email />
+                <Typography className="ms-2" variant="p">
+                  {user.email}
+                </Typography>
+              </div>
+            </>
+          ) : (
+            <>
+              <LinkButton label="Login" route="/login" />
+              <span>|</span>
+              <LinkButton label="Signup" route="/signup" />
+              <hr />
+            </>
+          )}
+          <div className="d-flex align-items-center justify-content-end">
+            <Switch checked={theme} color="default" onChange={onChangeTheme} />
+            {theme ? <Nightlight /> : <Brightness7 />}
           </div>
-        ) : (
-          <>
-            <LinkButton label="Login" route="/login" />
-            <span>|</span>
-            <LinkButton label="Signup" route="/signup" />
-          </>
-        )}
+        </div>
       </Toolbar>
       <Triangle />
     </AppBar>
--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,10 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-import "./assets/styles/index.scss";
-import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider, StyledEngineProvider } from "@mui/material";
-import theme from "./utils/theme/ThemeConfig";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme("light")}>
-        <StyledEngineProvider injectFirst>
-          <CssBaseline />
-          <App />
-        </StyledEngineProvider>
-      </ThemeProvider>
+      <App />
     </Provider>
   </React.StrictMode>
 );
